d0dd1afb99a6ac95f981f2477efa969797a41642
alexclp <alexandru.clapa@gmail.com>
2017-05-19 01:03:36 +0100

    Configured UICollectionView. Need to add action handlers and to test for different screen sizes

--- CursValutarSwift/BNRConverterViewController.swift
+++ CursValutarSwift/BNRConverterViewController.swift
@@ -9,49 +9,84 @@
 import UIKit
 
 class BNRConverterViewController: UIViewController {
+
+	fileprivate let kNumberOfRows = 4
+	fileprivate let kNumberOfColumns = 3
+	fileprivate let kNumberOfSections = 1
+	fileprivate let kCellSpacingTop: CGFloat = 20.0
+	fileprivate let kCellSpacingLeft: CGFloat = 0.5
+	fileprivate let kCellSpacingBottom: CGFloat = 20.0
+	fileprivate let kCellSpacingRight: CGFloat = 0.5
+	
+	fileprivate let collectionViewLabelsText = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "0", "< - >"]
 	
 	@IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		self.automaticallyAdjustsScrollViewInsets = false
+		configureCollectionViewLayout()
+		
+		self.view.backgroundColor = UIColor.hexStringToUIColor(hex: "DAC2AA")
+		collectionView.backgroundColor = UIColor.hexStringToUIColor(hex: "DAC2AA")
     }
+	
+	fileprivate func configureCollectionViewLayout() {
+		let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+		layout.sectionInset = UIEdgeInsets(top: kCellSpacingTop, left: kCellSpacingLeft, bottom: kCellSpacingBottom, right: kCellSpacingRight)
+		layout.minimumInteritemSpacing = 0
+		layout.minimumLineSpacing = 0
+		collectionView.collectionViewLayout = layout
+		
+		if let tabBar = tabBarController?.tabBar {
+			let adjustForTabbarInsets = UIEdgeInsetsMake(0, 0, tabBar.frame.height, 0)
+			collectionView.contentInset = adjustForTabbarInsets
+			collectionView.scrollIndicatorInsets = adjustForTabbarInsets
+		}
+	}
 }
 
 extension BNRConverterViewController: UICollectionViewDelegate {
-	
+	func collectionView(_ collectionView: UICollectionView,
+	                    layout collectionViewLayout: UICollectionViewLayout,
+	                    sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+		var collectionSize = collectionView.frame.size
+		if let tabBar = tabBarController?.tabBar {
+			collectionSize.height = collectionSize.height - tabBar.frame.height
+
+		}
+		
+		let itemWidth = collectionSize.width / CGFloat(kNumberOfColumns) - (kCellSpacingLeft + kCellSpacingRight) / CGFloat(kNumberOfColumns)
+		let itemHeight = collectionSize.height / CGFloat(kNumberOfRows) - (kCellSpacingTop + kCellSpacingBottom) / CGFloat(kNumberOfRows)
+		return CGSize(width: itemWidth, height: itemHeight)
+	}
 }
 
 extension BNRConverterViewController: UICollectionViewDataSource {
 	func numberOfSections(in collectionView: UICollectionView) -> Int {
-		return 1
+		return kNumberOfSections
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 12
+		return kNumberOfRows * kNumberOfColumns
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "buttonCell", for: indexPath)
-		cell.backgroundColor = UIColor.blue
+		
+		cell.backgroundColor = UIColor.white
+		cell.layer.borderColor = UIColor.hexStringToUIColor(hex: "D4D4D4").cgColor
+		cell.layer.borderWidth = 2.0
+		cell.layer.cornerRadius = 5
+		
+		let label = UILabel(frame: cell.bounds)
+		label.textAlignment = .center
+//		label.font = UIFont.boldSystemFont(ofSize: 20)
+		label.font = UIFont.systemFont(ofSize: 30, weight: UIFontWeightLight)
+		label.text = collectionViewLabelsText[indexPath.row]
+		cell.addSubview(label)
+		
 		return cell
-	}
-	
-	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-		let screenSize = UIScreen.main.bounds
-		let screenWidth = screenSize.width
-		let cellSquareSize: CGFloat = screenWidth / 2.0
-		return CGSize.init(width: cellSquareSize, height: cellSquareSize)
-	}
-	
-	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-		return UIEdgeInsetsMake(0, 0, 0.0, 0.0)
-	}
-	
-	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-		return 0.0
-	}
-	
-	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
-		return 0.0
 	}
 }
