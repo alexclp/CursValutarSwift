65d0debf6b213df406f7f5f4440fa3061129aa74
alexclp <alexandru.clapa@gmail.com>
2017-05-23 15:37:05 +0100

    PersistanceHelper tests

--- CursValutarSwiftTests/PersistanceHelperTests.swift
+++ CursValutarSwiftTests/PersistanceHelperTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import CursValutarSwift
 
 class PersistanceHelperTests: XCTestCase {
 
@@ -15,21 +16,12 @@ class PersistanceHelperTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+	func testFetchFullNamesShouldReturnADictionaryOfCurrencyCodeAndFullName() {
+		if let namesDict = PersistanceHelper.fetchFullNames() {
+			XCTAssertTrue(namesDict.keys.count == 31)
+			XCTAssertTrue(namesDict["USD"] == "Dolarul american")
+		} else {
+			XCTFail("Failed to fetch names from the file")
+		}
+	}
 }
